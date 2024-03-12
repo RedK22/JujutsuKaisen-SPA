d979e05e60c2a757872c46fc583e29a2cbb94ca1
Red22K <ritwik22k@gmail.com>
2024-03-12 16:04:34 +0530

    Fixed text errors

--- src/Pages/Itadori.jsx
+++ src/Pages/Itadori.jsx
@@ -6,7 +6,7 @@ function Itadori() {
     name: "Yuuji Itadori",
     image: ItadoriFB,
     description:
-      "Yuuji is a fair person who cares greatly for not only his comrades but anyone he views as people with their own wills, despite how deep or shallow his connection to them is. He cares greatly for the &quot;value of a life&quot; and to this end, he will ensure that others receive a &quot;proper death.&quot; He is easy to anger in the face of pure cruelty and unfair judgment of other people. He doesn&apos;t have the born talent required to use cursed techniques, but he has incredible athletic abilities and he is considered very strong for his age, as shown by when he easily beat a coach in Steel Ball Throw.",
+      "Yuuji is a fair person who cares greatly for not only his comrades but anyone he views as people with their own wills, despite how deep or shallow his connection to them is. He cares greatly for the value of a life and to this end, he will ensure that others receive a proper death. He is easy to anger in the face of pure cruelty and unfair judgment of other people. He doesn't have the born talent required to use cursed techniques, but he has incredible athletic abilities and is considered very strong for his age, as shown by when he easily beat a coach in Steel Ball Throw.",
     birthday: "Mar 20, 2003",
     gender: "Male",
     occupation: "Sorcerer, Student, Vessel",
